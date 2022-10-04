922cd798db3494ddae5d10852deacbb92dedb782
Aj007ar <aj007.ar@gmail.com>
2022-10-05 02:16:31 +0530

    [Ajay] added product review welcom message.

---
+++ ProductReview/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management system");
+        }
+    }
+}
