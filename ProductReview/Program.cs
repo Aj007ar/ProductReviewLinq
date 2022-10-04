8cfd925a96875879dac1b3b1742e6de4df438b7a
Aj007ar <aj007.ar@gmail.com>
2022-10-05 02:24:32 +0530

    [Ajay] added product review data

--- ProductReview/Program.cs
+++ ProductReview/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to product review management system");
+            Console.WriteLine("\n1.Add Values to list\nEnter Option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Creating a list for Product Review 
+            List<ProductReviews> productReviews = new List<ProductReviews>();
+            switch (option)
+            {
+                case 1:
+                    Products.AddingProductReview(productReviews);
+                    break;
+
+            }
         }
     }
 }
